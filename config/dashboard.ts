707dd25d934142ca47c56541db183f70fb25aebb
Manimukta Tiwari <manimukta.tiwari@pinnacle.in>
2025-10-06 10:38:56 +0000

    Feature/serp

--- config/dashboard.ts
+++ config/dashboard.ts
@@ -71,6 +71,13 @@ export const sidebarLinks: SidebarNavItem[] = [
         title: "E-mail",
         authorizeOnly: UserRole.ADMIN,
       },
+
+      {
+        href: "/dashboard/serpList",
+        icon: "logo",
+        title: "SERP",
+        authorizeOnly: UserRole.USER,
+      },
     ],
   },
   {
